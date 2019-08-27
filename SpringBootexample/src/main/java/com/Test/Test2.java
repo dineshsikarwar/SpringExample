335fb41ac8f7f5a815afaf79babfb5fd792c1d67
sensei <dinesh.s@sen-sei.in>
2019-08-27 11:50:43 +0530

    test1

--- SpringBootexample/src/main/java/com/Test/Test2.java
+++ SpringBootexample/src/main/java/com/Test/Test2.java
@@ -20,6 +20,7 @@ public class Test2 {
 	private static final String algorithm = "AES";
 	private static final String cipherPadding = "AES/CBC/PKCS5PADDING";
 	private static final int keySize = 128;
+        private static final String name="hello";
 
 	private static final String keyAsHexString = "2CB719C5DF693CA2C626EB1A02E9F2BD";
 
